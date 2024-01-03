749df5e00b77ea922c46ea33acf050b55608d22d
vldmkr <vladmakaryan@gmail.com>
2024-01-03 13:25:09 +0200

    Add support for ES256K algorithm in sign.js

--- examples/sign.js
+++ examples/sign.js
@@ -1,19 +1,23 @@
 const cose = require('../');
+const { secp256k1 } = require('@noble/curves/secp256k1');
 
 async function run () {
+  let COSEMessage;
+  const priv = secp256k1.utils.randomPrivateKey();
+  const pubkey = secp256k1.getPublicKey(priv);
   try {
     const plaintext = 'Important message!';
     const headers = {
-      p: { alg: 'ES256' },
-      u: { kid: '11' }
+      p: { alg: 'ES256K' }
     };
     const signer = {
       key: {
-        d: Buffer.from('6c1382765aec5358f117733d281c1c7bdc39884d04a45a1e6c67c858bc206c19', 'hex')
+        d: priv
       }
     };
-    const buf = await cose.sign.create(headers, plaintext, signer);
-    console.log('Signed message: ' + buf.toString('hex'));
+    COSEMessage = await cose.sign.create(headers, Buffer.from(plaintext), signer);
+    console.log('Public key: ' + Buffer.from(pubkey).toString('hex'));
+    console.log('Signed message: ' + COSEMessage.toString('hex'));
   } catch (error) {
     console.log(error);
   }
@@ -21,11 +25,9 @@ async function run () {
   try {
     const verifier = {
       key: {
-        x: Buffer.from('143329cce7868e416927599cf65a34f3ce2ffda55a7eca69ed8919a394d42f0f', 'hex'),
-        y: Buffer.from('60f7f1a780d8a783bfb7a2dd6b2796e8128dbbcef9d3d168db9529971a36e7b9', 'hex')
+        raw: pubkey
       }
     };
-    const COSEMessage = Buffer.from('d28443a10126a10442313172496d706f7274616e74206d6573736167652158404c2b6b66dfedc4cfef0f221cf7ac7f95087a4c4245fef0063a0fd4014b670f642d31e26d38345bb4efcdc7ded3083ab4fe71b62a23f766d83785f044b20534f9', 'hex');
     const buf = await cose.sign.verify(COSEMessage, verifier);
     console.log('Verified message: ' + buf.toString('utf8'));
   } catch (error) {
